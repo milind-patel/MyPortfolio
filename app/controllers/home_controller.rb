b6fedc34a182db87e8f7d854689e638f9b02a9d2
milind <milind@sasasoftwaretechnologies.com>
2016-09-21 13:49:09 +0530

    added changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
 	def index
+
+	end
+
+	def portfolio
+
+	end
+
+	def about
 		
+	end
+
+	def contact_us
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :home
-  
+  resources :home do
+    collection do
+      get "portfolio"
+      get "about"
+      get "contact_us"
+    end
+  end
+  root "home#index"
 end
